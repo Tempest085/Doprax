bd321759968e95d70dabc3b4db7fd44d125e9ea1
The Vampiro Elegant v2 <119989411+Tempest085@users.noreply.github.com>
2023-02-10 03:28:47 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone https://github.com/Tempest085/Vamplegendas okk && cd okk && pip3 install -U -r requirements.txt
-python3 channelbot.py
+git clone https://github.com/Thepan808/guard-bot okk && cd okk && pip3 install -U -r requirements.txt
+python3 -m eduu
