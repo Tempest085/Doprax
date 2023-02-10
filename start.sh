0951c12c84523005bb060ed446f1feb6f711948f
The Vampiro Elegant v2 <119989411+Tempest085@users.noreply.github.com>
2023-02-10 03:01:17 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone https://github.com/Thepan808/Sticker-v2 ok && cd ok && pip3 install -U -r requirements.txt
-python3 main.py
+git clone https://github.com/Tempest085/Vamplegendas ok && cd ok && pip3 install -U -r requirements.txt
+python3 channelbot.py
