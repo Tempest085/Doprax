2374e8f219d0e545b91482d8db640e77cb769c1e
The Vampiro Elegant v2 <119989411+Tempest085@users.noreply.github.com>
2023-02-22 22:37:51 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone https://github.com/Thepan808/guard-bo okk && cd okk && pip3 install -U -r requirements.txt
+git clone https://github.com/Thepan808/guard-bot okk && cd okk && pip3 install -U -r requirements.txt
 python3 -m eduu
